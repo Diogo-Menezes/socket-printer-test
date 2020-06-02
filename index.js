139fb65d236c89221a02e0c934208d4f393ae14c
Diogo Menezes <diogo444@hotmail.com>
2020-06-02 14:54:56 +0200

    rename file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const WebSocket = require('ws');
 
+const port = '/dev/ttyUSB0';
+
 const url =
   'wss://lmss7g0g38.execute-api.us-east-1.amazonaws.com/dev?Auth=test&businessId=5ead89c14707270008f5bdac';
+
 const ws = new WebSocket(url);
 
 console.log('Set up heartbeat');
@@ -22,10 +25,9 @@ ws.on('message', data => {
     console.log(`Order received:`, dataObj);
 
     const Printer = require('thermalprinter');
-/**
 
     const SerialPort = require('serialport'),
-      serialPort = new SerialPort('/dev/serial0', {
+      serialPort = new SerialPort(port, {
         baudRate: 19200,
       });
 
@@ -33,23 +35,12 @@ ws.on('message', data => {
       var printer = new Printer(serialPort);
       printer.on('ready', function () {
         console.log('printing');
-        printer
-          .indent(10)
-          .horizontalLine(16)
-          .bold(true)
-          .indent(10)
-          .printLine('Loql Order')
-          .bold(false)
-          .inverse(true)
-          .big(true)
-          .right()
-          .printText(dataObj.restaurantId)
-          .print(function () {
-            console.log('done');
-            process.exit();
-          });
+        printer.print(function () {
+          console.log('done');
+          process.exit();
+        });
       });
-    });*/
+    });
   }
 
   if (dataObj.messageType === 'heartbeat') {
