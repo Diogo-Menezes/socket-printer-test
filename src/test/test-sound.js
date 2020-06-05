2aede901a84559dbf5e5040ff155aa12908b4205
Diogo Menezes <diogo444@hotmail.com>
2020-06-05 15:33:05 +0200

    add volume

--- src/test/test-sound.js
+++ src/test/test-sound.js
@@ -4,7 +4,9 @@ const player = Omx('doorbell-2.mp3', 'local', false, 9);
 
 async function play() {
   try {
+    player.volUp(9);
     await player.play();
+    await player.quit();
     console.log('done');
   } catch (error) {
     console.error(error);
