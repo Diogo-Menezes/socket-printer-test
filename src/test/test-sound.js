17e615e33746671b164e52f70c028de1a449bdb6
Diogo Menezes <diogo444@hotmail.com>
2020-06-05 15:57:23 +0200

    add other dependecies

--- src/test/test-sound.js
+++ src/test/test-sound.js
@@ -1,18 +1,30 @@
 const Omx = require('node-omxplayer');
-
+const soundPlayer = require('play-sound');
+const playerSound = require('sound-play');
 const player = Omx('./doorbell-2.mp3');
 
 async function play() {
   try {
     player.volUp(9);
     await player.play();
-  
+
     player.on('close', () => console.log('finished'));
   } catch (error) {
     console.error(error);
   }
 }
+function play2() {
+  soundPlayer.play('./media/roadrunner.mp3', err => {
+    if (err) console.log(`Could not play sound: ${err}`);
+  });
+}
 
-play();
+function play3() {
+  playerSound.play('./media/roadrunner.mp3', err => {
+    if (err) console.log(`Could not play sound: ${err}`);
+  });
+}
 
-
+// play();
+play2();
+// play3();
