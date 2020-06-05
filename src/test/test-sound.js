ebcf8c9519bd8f62ea00b5690dbb984188613637
Diogo Menezes <diogo444@hotmail.com>
2020-06-05 15:51:23 +0200

    remove timeout

--- src/test/test-sound.js
+++ src/test/test-sound.js
@@ -6,11 +6,7 @@ async function play() {
   try {
     player.volUp(9);
     await player.play();
-    setTimeout(() => {
-      player.quit();
-      console.log('done');
-    }, 3000);
-
+  
     player.on('close', () => console.log('finished'));
   } catch (error) {
     console.error(error);
