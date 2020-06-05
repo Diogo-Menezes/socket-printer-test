5d17092ff55b05e93e2d7f6211c0c782192efa67
Diogo Menezes <diogo444@hotmail.com>
2020-06-05 15:14:44 +0200

    add test function

--- src/test/test-sound.js
+++ src/test/test-sound.js
@@ -1,9 +1,14 @@
 const Omx = require('node-omxplayer');
 
 const player = Omx('doorbell-2.mp3');
-try {
-  await player.play();
-  console.log('done');
-} catch (error) {
-  console.error(error);
+
+async function play() {
+  try {
+    await player.play();
+    console.log('done');
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+play();
