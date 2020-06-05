c2a3ca753c87db5ccba653db31c8fda70f9f5fd5
Diogo Menezes <diogo444@hotmail.com>
2020-06-05 15:36:12 +0200

    add timeout and loop

--- src/test/test-sound.js
+++ src/test/test-sound.js
@@ -1,13 +1,17 @@
 const Omx = require('node-omxplayer');
 
-const player = Omx('doorbell-2.mp3', 'local', false, 9);
+const player = Omx('doorbell-2.mp3', 'local', true, 9);
 
 async function play() {
   try {
     player.volUp(9);
     await player.play();
-    await player.quit();
-    console.log('done');
+    setTimeout(() => {
+      player.quit();
+      console.log('done');
+    }, 3000);
+
+    player.on('close', console.log('finished'));
   } catch (error) {
     console.error(error);
   }
