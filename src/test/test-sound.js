2dd0d7afee1679ed586012d1587c6fc30832aa94
Diogo Menezes <diogo444@hotmail.com>
2020-06-08 08:47:54 +0200

    removed unused dep

--- src/test/test-sound.js
+++ src/test/test-sound.js
@@ -1,30 +1,9 @@
-const Omx = require('node-omxplayer');
 var soundPlayer = require('play-sound')();
-const playerSound = require('sound-play');
-const player = Omx('./doorbell-2.mp3');
 
 async function play() {
-  try {
-    player.volUp(9);
-    await player.play();
-
-    player.on('close', () => console.log('finished'));
-  } catch (error) {
-    console.error(error);
-  }
-}
-async function play2() {
   await soundPlayer.play('doorbell-2.mp3', { omxplayer: ['--vol', 9] }, err => {
     if (err) console.log(`Could not play sound: ${err}`);
   });
 }
 
-function play3() {
-  playerSound.play('doorbell-2.mp3', err => {
-    if (err) console.log(`Could not play sound: ${err}`);
-  });
-}
-
-// play();
-play2();
-// play3();
+play();
