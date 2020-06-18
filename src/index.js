53f39da2e17fb6fb78014cfdffe1140f419bc838
Diogo Menezes <diogo444@hotmail.com>
2020-06-18 15:02:21 +0200

    split files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const ReconnectingWebSocket = require('reconnecting-websocket');
-const Websocket = require('ws');
+const WS = require('ws');
 const SerialPort = require('serialport');
 const Printer = require('thermalprinter');
 const sendToPrint = require('./utils/print');
@@ -9,88 +9,52 @@ const port = '/dev/serial0';
 const secret = 'serious';
 const id = '5edbead255ded300082a3724';
 
-const secret1 = 'test';
-const id1 = '5ead89c14707270008f5bdac';
+const url = `  wss://lmss7g0g38.execute-api.us-east-1.amazonaws.com/dev?Auth=${secret}&businessId=${id}`;
 
 const options = {
-  WebSocket: Websocket,
+  WebSocket: WS,
   minUptime: 7200000, //2h
-  connectionTimeout: 1000,
-  maxRetries: 10,
+  connectionTimeout: 7000000,
 };
 
-const url = `  wss://lmss7g0g38.execute-api.us-east-1.amazonaws.com/dev?Auth=${secret}&businessId=${id}`;
-const ws = new ReconnectingWebSocket(url, [], options);
+const rws = new ReconnectingWebSocket(url, [], options);
 
-console.log('Set up heartbeat');
-setInterval(() => {
-  console.log('Heartbeat sent');
-  const message = JSON.stringify({
-    action: 'echo',
-    data: { messageType: 'heartbeat' },
-  });
-  ws.send(message);
-}, 540000);
-
-var timerID = 0;
-
-function keepAlive() {
-  var timeout = 20000;
-
-  if (ws.readyState == ws.OPEN) {
-    console.log('Sent keep awake');
-    ws.send('');
-  }
-
-  timerId = setTimeout(keepAlive, timeout);
-}
-
-ws.on('open', () => {
-  console.log('connected');
-  keepAlive();
+rws.addEventListener('open', () => {
+  rws.send('hello!');
 });
 
-ws.on('message', data => {
+rws.addEventListener('message', ({ data }) => {
   const dataObj = JSON.parse(data);
-  if (dataObj.messageType === 'order') {
-    console.log(`Order received:`, JSON.stringify(dataObj, null, 2));
 
-    try {
-      serialPort = new SerialPort(port, { baudRate: 19200 });
+  console.log(dataObj.message);
 
-      serialPort.on('open', function () {
-        var printer = new Printer(serialPort);
-
-        printer.on('ready', async function () {
-          console.log('printer ready');
-
-          await ring();
-
-          sendToPrint(printer, dataObj);
-
-          sendToPrint(printer, dataObj);
-
-          printer.print();
-        });
-      });
-    } catch (error) {
-      console.log(error);
+  try {
+    if (dataObj.message === 'order') {
+      console.log(`Order received:`, JSON.stringify(dataObj, null, 2));
     }
-  }
+    const serialPort = new SerialPort(port, { baudRate: 19200 });
 
-  if (dataObj.messageType === 'heartbeat') {
-    console.log('Heartbeat received');
+    serialPort.on('open', function () {
+      var printer = new Printer(serialPort);
+
+      printer.on('ready', async function () {
+        console.log('printer ready');
+
+        await ring();
+
+        sendToPrint(printer, dataObj);
+
+        sendToPrint(printer, dataObj);
+
+        printer.print();
+      });
+    });
+  } catch (error) {
+    console.log(error);
   }
 });
 
-ws.on('close', () => {
-  console.log('disconnected');
-  cancelKeepAlive();
+rws.addEventListener('close', () => {
+  console.log('disconnect');
   // process.exit();
 });
-
-function cancelKeepAlive() {
-  if (timerId) {
-    clearTimeout(timerId);
-  }
-}
---
+++ src/ws.js
@@ -0,0 +1,88 @@
+const WebSocket = require('ws');
+const SerialPort = require('serialport');
+const Printer = require('thermalprinter');
+const sendToPrint = require('./utils/print');
+const ring = require('./utils/ring');
+const port = '/dev/serial0';
+
+const secret = 'serious';
+const id = '5edbead255ded300082a3724';
+
+const secret1 = 'test';
+const id1 = '5ead89c14707270008f5bdac';
+
+const url = `  wss://lmss7g0g38.execute-api.us-east-1.amazonaws.com/dev?Auth=${secret}&businessId=${id}`;
+const ws = new WebSocket(url);
+
+console.log('Set up heartbeat');
+setInterval(() => {
+  console.log('Heartbeat sent');
+  const message = JSON.stringify({
+    action: 'echo',
+    data: { messageType: 'heartbeat' },
+  });
+  ws.send(message);
+}, 540000);
+
+var timerID = 0;
+
+function keepAlive() {
+  var timeout = 20000;
+
+  if (ws.readyState == ws.OPEN) {
+    console.log('Sent keep awake');
+    ws.send('');
+  }
+
+  timerId = setTimeout(keepAlive, timeout);
+}
+
+ws.on('open', () => {
+  console.log('connected');
+  keepAlive();
+});
+
+ws.on('message', data => {
+  const dataObj = JSON.parse(data);
+  if (dataObj.messageType === 'order') {
+    console.log(`Order received:`, JSON.stringify(dataObj, null, 2));
+
+    try {
+      serialPort = new SerialPort(port, { baudRate: 19200 });
+
+      serialPort.on('open', function () {
+        var printer = new Printer(serialPort);
+
+        printer.on('ready', async function () {
+          console.log('printer ready');
+
+          await ring();
+
+          sendToPrint(printer, dataObj);
+
+          sendToPrint(printer, dataObj);
+
+          printer.print();
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (dataObj.messageType === 'heartbeat') {
+    console.log('Heartbeat received');
+  }
+});
+
+ws.on('close', () => {
+  console.log('disconnected');
+  cancelKeepAlive();
+  process.exit();
+});
+
+function cancelKeepAlive() {
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+}
